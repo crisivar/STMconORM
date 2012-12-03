228701144216a337841c8891fa5a320464f918c7
Cristian Rojas <crisivar@gmail.com>
2012-12-02 21:12:21 -0500

    agregado el controlador de bus con las operaciones CRUD basicas
    aun no estan parametrizadas pero basicamente asi se debe implementar el
    metodo

--- STM/Interfaz_STM/Program.cs
+++ STM/Interfaz_STM/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using ORM_STM.Controladores;
 
 namespace Interfaz_STM
 {
@@ -16,6 +17,18 @@ namespace Interfaz_STM
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            ControladorBus contrB = new ControladorBus();
+
+            contrB.AñadirBus(1111, new DateTime(2010, 1, 18), "asdsd", "asdasd", "asdasd", "asdasd", "adsgfdfg", "fghdgh", "dfhdgh", true);
+
+            ControladorBus contrEditar = new ControladorBus();
+
+            //aca hay un problema que detecta llave duplicada en la base de datos :S
+            contrEditar.EditarBus(new DateTime(2010, 1, 18), "ggggg", "gggggg", "gggggg", "asdasd", "adsgfdfg", "fghdgh", "dfhdgh", true);
+
+
+
         }
     }
 }
---
+++ STM/ORM_STM/Controladores/ControladorBus.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ORM_STM.Controladores
+{
+    
+    public class ControladorBus
+    {
+
+        public void AñadirBus(int Id, DateTime Fecha_adq, String modelo, String placa, String tipo, String color, String fabricante, String capacidad, String tipo_combust, Boolean estado)
+        {
+
+            using (EntitiesModel dbContext = new EntitiesModel())
+            {
+
+                Bu bus = new Bu();
+                bus.Id = Id;
+                bus.Fecha_adq = Fecha_adq;
+                bus.Modelo = modelo;
+                bus.Placa = placa;
+                bus.Tipo = tipo;
+                bus.Color = color;
+                bus.Fabricante = fabricante;
+                bus.Capacidad = capacidad;
+                bus.Tipo_combust = tipo_combust;
+                bus.Estado = estado;
+
+                dbContext.Add(bus);
+                dbContext.SaveChanges();
+
+            }
+
+
+        }
+
+        public void EditarBus(DateTime Fecha_adq, String modelo, String placa, String tipo, String color, String fabricante, String capacidad, String tipo_combust, Boolean estado)
+        {
+
+            using (EntitiesModel dbContext = new EntitiesModel())
+            {
+
+                //se supone que debemos hacer el update pidiendo el ID.....falta implementarlo
+
+                Bu busParaEditar = dbContext.Bus.First();
+                               
+                busParaEditar.Fecha_adq = Fecha_adq;
+                busParaEditar.Modelo = modelo;
+                busParaEditar.Placa = placa;
+                busParaEditar.Tipo = tipo;
+                busParaEditar.Color = color;
+                busParaEditar.Fabricante = fabricante;
+                busParaEditar.Capacidad = capacidad;
+                busParaEditar.Tipo_combust = tipo_combust;
+                busParaEditar.Estado = estado;
+
+                dbContext.SaveChanges();
+
+            }
+                   
+        }
+
+        public void EliminarBus()
+        {
+
+            using (EntitiesModel dbContext = new EntitiesModel())
+            {
+
+                Bu BusParaBorrar = dbContext.Bus.Last();
+                dbContext.Delete(BusParaBorrar);
+                dbContext.SaveChanges();
+
+            }
+        
+        }
+
+        public void ConsultaBusFull() //toca parametrizarlo
+        {
+            
+            using (EntitiesModel dbContext = new EntitiesModel())
+            {
+                IEnumerable<Bu> bus = dbContext.Bus.ToList();
+            }
+            
+        }
+
+        public void ConsultaBusPorId(int id)
+        {
+
+            using (EntitiesModel dbContext = new EntitiesModel())
+            {
+                IEnumerable<Bu> categories = dbContext.Bus.Where(
+                    b =>  b.Id== id);
+            }
+        
+        }
+
+        public void ConsultaOrdenada() //es un ejemplo...podemos ponerle un parametro de entrada
+        {
+
+            using (EntitiesModel dbContext = new EntitiesModel())
+            {
+                List<Bu> buses = (from Bus in dbContext.Bus
+                                  orderby Bus.Placa
+                                  select Bus).ToList();
+            }
+        
+        }
+
+
+
+    }
+}
